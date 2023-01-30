bb0b92b2308d7ddc3af1222022eb1bc5f0fc5c46
Carlos Villarreal <Carlos.Villarreal@solera.com>
2023-01-30 17:13:14 +0100

    first commit

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useCounter } from '../hooks/useCounter';
+
+export const  CounterWithCustomHook = () => {
+
+    const { counter, increment, decrement, reset } = useCounter();
+
+  return (
+    <div>
+        <h1>Counter with Custom Hook: { counter }</h1>
+        <hr />
+
+        <button className='btn btn-primary' onClick={ increment }>+1</button>
+        <button className='btn btn-primary' onClick={ reset }>Reset</button>
+        <button className='btn btn-primary' onClick={ decrement }>-1</button>
+    </div>
+  )
+}
